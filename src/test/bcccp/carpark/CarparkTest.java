a97c92a592c386728c0166869cd6c9708473df6c
rkohli <rupakkohli@gmail.com>
2017-09-21 20:42:32 +1000

    [Test] Added tests for capacity and name.

--- src/test/bcccp/carpark/CarparkTest.java
+++ src/test/bcccp/carpark/CarparkTest.java
@@ -1,33 +1,68 @@
 package test.bcccp.carpark;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
 
 import bcccp.carpark.Carpark;
 import bcccp.tickets.adhoc.IAdhocTicketDAO;
 import bcccp.tickets.season.ISeasonTicketDAO;
 
-import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 public class CarparkTest {
 
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
 	private IAdhocTicketDAO adhocTicketMock_ = mock(IAdhocTicketDAO.class);
 	private ISeasonTicketDAO seasonTicketMock_ = mock(ISeasonTicketDAO.class);
 	
-	@Test(expected=RuntimeException.class)
+	@Test
 	public void TestCarparkNameNull() {
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage(containsString("name"));
 		new Carpark(null, 1, this.adhocTicketMock_, this.seasonTicketMock_);
-		fail();
 	}
 	
-	@Test(expected=RuntimeException.class)
+	@Test
 	public void TestCarparkNameEmpty() {
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage(containsString("name"));
 		new Carpark("", 1, this.adhocTicketMock_, this.seasonTicketMock_);
-		fail();
 	}
 	
-	@Test(expected=RuntimeException.class)
+	@Test
 	public void TestCarparkNameEmptySpace() {
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage(containsString("name"));
 		new Carpark(" ", 1, this.adhocTicketMock_, this.seasonTicketMock_);
-		fail();
+	}
+	
+	@Test
+	public void TestCarparkNegativeCapacity() {
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage(containsString("capacity"));
+		new Carpark("Name", -1, this.adhocTicketMock_, this.seasonTicketMock_);
+	}
+	
+	@Test
+	public void TestCarparkZeroCapacity() {
+		expectedException.expect(RuntimeException.class);
+		expectedException.expectMessage(containsString("capacity"));
+		new Carpark("Name", 0, this.adhocTicketMock_, this.seasonTicketMock_);
+	}
+	
+	@Test
+	public void TestGetName() {
+		Carpark carpark = new Carpark("Name", 1, this.adhocTicketMock_, this.seasonTicketMock_);
+		assertEquals("Name", carpark.getName());
+	}
+	
+	@Test
+	public void TestGetCapacity() {
+		Carpark carpark = new Carpark("Name", 5, this.adhocTicketMock_, this.seasonTicketMock_);
+		assertEquals(5, carpark.getCapacity());
 	}
 }
