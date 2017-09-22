3f8537ff475491a7b550bed355def6a1cd46cd1f
rkohli <rupakkohli@gmail.com>
2017-09-22 10:08:34 +1000

    [Test] end of business.

--- src/test/bcccp/carpark/ChargeCalculatorTest.java
+++ src/test/bcccp/carpark/ChargeCalculatorTest.java
@@ -34,4 +34,10 @@ public class ChargeCalculatorTest {
 		double minutes = 5 * 60 + 59;
 		assertEquals(beforeStart, minutes * 2, DELTA);
 	}
+	
+	@Test
+	public void testAfterEndOfBusiness() {
+		double beforeStart = ChargeCalculator.calcDayCharge(LocalTime.of(19, 0, 1), LocalTime.of(20, 0, 1), DayOfWeek.MONDAY);
+		assertEquals(beforeStart, 60 * 2, DELTA);
+	}
 }
