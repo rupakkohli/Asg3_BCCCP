484527acfc945a044be5b1df36317466ea8b879d
JunaidKhan17 <Asadlife89@gmail.com>
2017-09-22 15:10:51 +1000

    Fixing up code formatting and finishing integration testing for paystation

--- src/test/bcccp/carpark/integration/PaystationIntegrationTest.java
+++ src/test/bcccp/carpark/integration/PaystationIntegrationTest.java
@@ -7,65 +7,93 @@ import org.junit.Before;
 import org.junit.Test;
 
 import bcccp.carpark.Carpark;
+import bcccp.carpark.TimeProvider;
 import bcccp.carpark.paystation.PaystationController;
 import bcccp.carpark.paystation.PaystationUI;
 import bcccp.carpark.paystation.PaystationController.STATE;
 import bcccp.tickets.adhoc.AdhocTicketDAO;
 import bcccp.tickets.adhoc.AdhocTicketFactory;
 import bcccp.tickets.adhoc.IAdhocTicketDAO;
+import bcccp.tickets.season.SeasonTicketDAO;
+import bcccp.tickets.season.UsageRecordFactory;
 
 public class PaystationIntegrationTest {
 
-	private Carpark myCarpark;
-	private AdhocTicketFactory myFactory;
-	private AdhocTicketDAO myAdhocDAO;
-	
-	
-	
-	private PaystationUI myPUI;
-	private PaystationController myController;
+	private Carpark myCarpark_;
+	private AdhocTicketFactory myFactory_;
+	private AdhocTicketDAO myAdhocDAO_;
+	private SeasonTicketDAO mySeasonDAO_;
+	private UsageRecordFactory myUsageFactory_;
+
+	private PaystationUI myPUI_;
+	private PaystationController myController_;
+	private TimeProvider mTimeProvider_;
 	
 	@Before
 	public void setUp() throws Exception {
 		// Setting up AdhocDAO, AdhocTicketFactory
-		myFactory = new AdhocTicketFactory();
-		myAdhocDAO = new AdhocTicketDAO(myFactory);
+		myFactory_ = new AdhocTicketFactory();
+		myAdhocDAO_ = new AdhocTicketDAO(myFactory_);
+		
+		myUsageFactory_ = new UsageRecordFactory();
+		mySeasonDAO_ = new SeasonTicketDAO(myUsageFactory_);
+		mTimeProvider_ = mock(TimeProvider.class);
+		
 	}
 	
 	@Test
-	public void testTicketInsertedSuccess() {
+	public void TestPaystationControllerIntegration(){
 		
 		// Setup carpark, UI and Controller
 		
-		myCarpark = new Carpark("Carpark", 2, myAdhocDAO, null);
-		myPUI = new PaystationUI(0,0);
-		myController = new PaystationController(myCarpark, myPUI);
+		myCarpark_ = new Carpark("Carpark", 10, myAdhocDAO_, mySeasonDAO_, mTimeProvider_);
+		myPUI_ = new PaystationUI(0,0);
+		myController_ = new PaystationController(myCarpark_, myPUI_);
 		
 		// First ticket created should be A1
-		myCarpark.issueAdhocTicket();
-		myController.ticketInserted("A1");
+		myCarpark_.issueAdhocTicket();
+		myController_.ticketInserted("A1");
 		
-		assertEquals(myController.getState(), STATE.WAITING);
-		myController.ticketPaid();
-		assertEquals(myController.getState(), STATE.PAID);	
-		myController.ticketTaken();
-		assertEquals(myController.getState(), STATE.IDLE);
+		// Test full state diagram from idle to paid to idle
+		assertEquals(myController_.getState(), STATE.WAITING);
+		myController_.ticketPaid();
+		assertEquals(myController_.getState(), STATE.PAID);	
+		myController_.ticketTaken();
+		assertEquals(myController_.getState(), STATE.IDLE);
+		
+		//Test an invalid ticket
+		
+		myController_.ticketInserted("A3"); // Hasn't been created yet so invalid
+		
+		assertEquals(myController_.getState(), STATE.REJECTED);
+		assertEquals(myPUI_.getDisplayText(), "Take Rejected Ticket");
+		myController_.ticketTaken();
+		assertEquals(myController_.getState(), STATE.IDLE);
+		
+		myCarpark_.issueAdhocTicket();
+		myCarpark_.issueAdhocTicket();
+		
+		// Should be up to A3 now
+		
+		myController_.ticketInserted("A3");
+		assertEquals(myController_.getState(), STATE.WAITING);
+		myController_.ticketTaken();
+		assertEquals(myController_.getState(), STATE.IDLE);
+		
+		// Pay in invalid state
+		myController_.ticketPaid();
+		assertNotEquals(myController_.getState(), STATE.PAID);
+		myController_.ticketInserted("A3");
+		myController_.ticketPaid();
+		assertEquals(myController_.getState(), STATE.PAID);	
+		myController_.ticketTaken();
+		assertEquals(myController_.getState(), STATE.IDLE);
+		
 
 	}
 	
-	@Test
-	public void testTicketInsertedRejected() {
-		myCarpark = new Carpark("Carpark", 2, myAdhocDAO, null);
-		myPUI = new PaystationUI(0,0);
-		myController = new PaystationController(myCarpark, myPUI);
-		
-		myCarpark.issueAdhocTicket();
-		myController.ticketInserted("A3"); // Hasn't been created yet so invalid
-		
-		assertEquals(myController.getState(), STATE.REJECTED);
-		myController.ticketTaken();
-		assertEquals(myController.getState(), STATE.IDLE);
-	}
+	
+	
 
 
 	
